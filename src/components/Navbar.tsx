08a0a372c3c20d91e5bcd0e8baa3e9ea46d7d511
drimescodes <olamidebamigbola@student.oauife.edu.ng>
2024-06-08 10:06:27 +0100

    Update Navbar.tsx

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,4 +1,3 @@
-import React from "react";
 import { Link } from "react-router-dom";
 import { Button } from "@/components/ui/button";
 import { SheetTrigger, SheetContent, Sheet } from "@/components/ui/sheet";
